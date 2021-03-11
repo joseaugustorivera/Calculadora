8d6cd6f77515e84c7cb6ed871215aa94cd33bef3
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-03-11 13:48:58 -0400

    Se creo el controlador Calculator y se crearon las rutas ADD, MULTIPLY, SUBTRACT y DIVIDE en POST

---
+++ SolCalculadora/ApiCalculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿namespace ApiCalculadora.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a + num_b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a * num_b;
+        }
+        [HttpPost]
+        [Route("Subtract")]
+        public int Subtract([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a - num_b;
+        }
+        [HttpPost]
+        [Route("Divide")]
+        public int Divide([FromHeader] int num_a, [FromHeader] int num_b)
+        {
+            return num_a / num_b;
+        }
+    }
+}
